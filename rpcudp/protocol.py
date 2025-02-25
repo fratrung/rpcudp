f72b36ca4ed11e428772dba01e01bc64b93fe75d
ciccio.trung <francesco.trung@gmail.com>
2025-02-25 10:03:01 +0100

    import fixed

--- rpcudp/protocol.py
+++ rpcudp/protocol.py
@@ -9,7 +9,7 @@ from hashlib import sha1
 
 import umsgpack
 
-from rpcudp.rpcudp.exceptions import MalformedMessage
+from rpcudp.exceptions import MalformedMessage
 
 LOG = logging.getLogger(__name__)
 
